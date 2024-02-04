20752ccea00070f3163630371c4fc641d9b8951e
Sed-93 <sedat-933@hotmail.se>
2024-02-04 17:12:20 +0100

    Lagt till controller, startup & program.cs

---
+++ WebApplication1/Controllers/controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EncryptionDecryptionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CryptoController : ControllerBase
+    {
+        private static readonly byte[] Key = Encoding.UTF8.GetBytes("A key for AES");
+        private static readonly byte[] IV = Encoding.UTF8.GetBytes("An IV for AES");
+
+        [HttpPost("encrypt")]
+        public ActionResult<string> Encrypt([FromBody] string plainText)
+        {
+            using var aes = Aes.Create();
+            aes.Key = Key;
+            aes.IV = IV;
+
+            var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using var msEncrypt = new MemoryStream();
+            using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+            using (var swEncrypt = new StreamWriter(csEncrypt))
+            {
+                swEncrypt.Write(plainText);
+            }
+
+            var encrypted = msEncrypt.ToArray();
+            return Convert.ToBase64String(encrypted);
+        }
+
+        [HttpPost("decrypt")]
+        public ActionResult<string> Decrypt([FromBody] string cipherText)
+        {
+            var fullCipher = Convert.FromBase64String(cipherText);
+
+            using var aes = Aes.Create();
+            aes.Key = Key;
+            aes.IV = IV;
+
+            var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using var msDecrypt = new MemoryStream(fullCipher);
+            using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using var srDecrypt = new StreamReader(csDecrypt);
+
+            var decrypted = srDecrypt.ReadToEnd();
+            return decrypted;
+        }
+    }
+}
